a81ca159a8f2e82f0e311306edb5c7b01a260501
Deoclécio Filho <deoclecio@ice.ufjf.br>
2019-08-22 19:53:31 -0300

    Adiciona controle por tecla.

--- Sprite.js
+++ Sprite.js
@@ -1,5 +1,5 @@
-function Sprite(exemplo) {
-    var{
+function Sprite(exemplo = {}) {
+    var {
         x = 10,
         y = 100,
         w = 10,
@@ -7,7 +7,7 @@ function Sprite(exemplo) {
         vy = 0,
         color = "blue"
     } = exemplo;
-    
+
     this.x = x;
     this.y = y;
 
@@ -22,29 +22,40 @@ function Sprite(exemplo) {
 Sprite.prototype = new Sprite({});
 Sprite.constructor = Sprite;
 
-Sprite.prototype.desenhar = function(ctx) {
-    ctx.fillStyle = "color";
+Sprite.prototype.desenhar = function (ctx) {
+    ctx.fillStyle = this.color;
     ctx.strokeStyle = "black";
     ctx.fillRect(this.x, this.y, this.w, this.w);
 }
 
-Sprite.prototype.mover = function(dt) {
+Sprite.prototype.mover = function (dt) {
     this.x = this.x + this.vx * dt;
     this.y = this.y + this.vy * dt;
 }
 
-Sprite.prototype.colidiuCom = function(alvo){
-    if(alvo.x + alvo.w < this.x) return false;
-    if(alvo.x > this.x + this.w) return false;
+Sprite.prototype.colidiuCom = function (alvo) {
+    if (alvo.x + alvo.w < this.x) return false;
+    if (alvo.x > this.x + this.w) return false;
 
-    if(alvo.y + alvo.w < this.y) return false;
-    if(alvo.y > this.y + this.w) return false;
+    if (alvo.y + alvo.w < this.y) return false;
+    if (alvo.y > this.y + this.w) return false;
 
     return true;
 
 }
 
-Sprite.prototype.perseguir =function(alvo){
-    this.vx = 40*Math.sign(alvo.x - this.x);
-    this.vy = 40*Math.sign(alvo.y - this.y); 
+Sprite.prototype.perseguir = function (opcoes) {
+    this.vx = 40 * Math.sign(opcoes.alvo.x - this.x);
+    this.vy = 40 * Math.sign(opcoes.alvo.y - this.y);
+}
+
+Sprite.prototype.controlePorTeclas = function (opcoes) {
+    if (opcoes.teclas.esquerda) { this.vx = -50; }
+    if (opcoes.teclas.direita) { this.vx = 50; }
+    if (!opcoes.teclas.esquerda && !opcoes.teclas.direita)
+        this.vx = 0;
+    if (opcoes.teclas.cima) { this.vy = -50; }
+    if (opcoes.teclas.baixo) { this.vy = 50; }
+    if (!opcoes.teclas.cima && !opcoes.teclas.baixo)
+    this.vy = 0;
 }
